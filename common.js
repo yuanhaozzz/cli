d13850dfeb5f65883d67d75a287b8255ee49ad66
yuanhao <huzhen@inke.cn>
2022-05-18 15:59:24 +0800

    deploy

---
+++ common.js
@@ -0,0 +1,15 @@
+function logSuccess(arrSucc = []) {
+  console.log(
+    "🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉"
+  );
+  console.log(`🎉 `);
+  arrSucc.forEach((succ) => console.log(`🎉 ` + succ));
+  console.log(`🎉 `);
+  console.log(
+    "🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉"
+  );
+}
+
+module.exports = {
+  logSuccess,
+};
---
+++ constant.js
@@ -0,0 +1,22 @@
+module.exports = {
+  template: "https://github.com/yuanhaozzz/react-cli-template",
+};
+
+module.exports = [
+  {
+    name: "react pc端 单页应用",
+    url: "https://github.com/yuanhaozzz/react-cli-template",
+  },
+  {
+    name: "react pc端 后台管理系统",
+    url: "https://github.com/yuanhaozzz/react-cli-admin-pc.git",
+  },
+  {
+    name: "react 移动端 单页应用",
+    url: "https://github.com/yuanhaozzz/react-cli-single-mobile.git",
+  },
+  {
+    name: "react 移动端 多页应用 按需打包",
+    url: "https://github.com/yuanhaozzz/react-cli-single-mobile.git",
+  },
+];
